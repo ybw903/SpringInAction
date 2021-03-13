cc001f97c7238f3df932272875565d9d5c847f3b
병우 유 <conner903@naver.com>
2021-03-13 17:44:02 +0900

    CHAP03-2 스프링 데이터 JPA를 사용해서 데이터 저장하고 사용하기

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,7 +1,18 @@
 package tacos.data;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
 
-public interface OrderRepository {
-    Order save(Order order);
+import java.util.Date;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+    //Order save(Order order);
+    List<Order> findByDeliveryZip(String deliveryZip);
+
+    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    @Query("Order o where o.deliveryCity='Seattle'")
+    List<Order> readOrdersDeliveredInSeattle();
 }
